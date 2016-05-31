575ac7bb59667d0cd6acfca0f3d5022276ab973e
zss1980 <szautkin@gmail.com>
2016-05-31 12:16:53 -0500

    Adjusting mob view of map with JQuery

--- public/js/scriptIndex.js
+++ public/js/scriptIndex.js
@@ -21,6 +21,31 @@ window.addEventListener('load', function () {
 
 ready: function(){
   this.getView();
+  $(".collapse").on('show.bs.collapse', function(){
+    $('body').scrollTo('#map',{duration:'1000', offsetTop : '50'});
+    });
+
+  $(".collapse").on('click', function(){
+    $(".collapse").collapse('toggle');
+    });
+ 
+  $.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
  
   
   	
