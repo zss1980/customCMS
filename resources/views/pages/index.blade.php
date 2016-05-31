0fd07f26f732e27804735086e306bc5b9a2d91d7
zss1980 <szautkin@gmail.com>
2016-05-31 06:18:28 -0500

    copyright captions corrected

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -174,7 +174,7 @@ Test
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        Copyright &copy; {!!$dataFooter->copyrightText!!}<br> <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                        Copyright &copy; {!!$dataFooter->copyrightText!!}<br>
                     </div>
                 </div>
             </div>
